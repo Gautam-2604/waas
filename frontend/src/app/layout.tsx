cc81af4977a84e0ff42f791f1da85c0041e126a9
Gautam-2604 <gaurisariag@gmail.com>
2024-08-07 22:53:23 +0530

    Hero section made here

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Appbar } from "./components/Appbar";
 import { Providers } from "./providers";
+import { Hero } from "./components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
         <Appbar />
+        <Hero />
         {children}
         </Providers>
       
