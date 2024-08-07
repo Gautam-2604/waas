ab87e7692280c9c94112562d4fa3ad687e1b4e38
Gautam-2604 <gaurisariag@gmail.com>
2024-08-07 23:01:39 +0530

    some changes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
         <Appbar />
-        <Hero />
         {children}
         </Providers>
       
