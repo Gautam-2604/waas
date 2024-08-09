1739a5a427cf3d410f09d875752ad0c5921f480a
Gautam-2604 <gaurisariag@gmail.com>
2024-08-09 12:43:55 +0530

    did sign in correctly

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,4 +11,5 @@ export const authOptions = {
   ]
 }
 
-export default NextAuth(authOptions)
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
